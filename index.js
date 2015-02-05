d91adbd183a1b7b44f3ee0063df1ff5607496f13
Andrey Sitnik <andrey@sitnik.ru>
2015-02-05 08:33:55 +0300

    Add base support for size property

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function (css) {
+var list = require('postcss/lib/list');
 
+module.exports = function (css) {
+    css.eachDecl('size', function (decl, i) {
+        var sizes = list.space(decl.value);
+        if ( sizes.length == 1 ) sizes[1] = sizes[0];
+
+        decl.cloneBefore({ prop: 'width',  value: sizes[0] });
+        decl.cloneBefore({ prop: 'height', value: sizes[1] });
+
+        decl.removeSelf();
+    });
 };
--- test/test.js
+++ test/test.js
@@ -9,4 +9,17 @@ var test = function (input, output) {
 
 describe('postcss-size', function () {
 
+    it('sets width and height', function () {
+        test('a{ size: 1px 2px; }', 'a{ width: 1px; height: 2px; }');
+    });
+
+    it('sets width and height by one value', function () {
+        test('a{ size: 1px; }', 'a{ width: 1px; height: 1px; }');
+    });
+
+    it('splits values by brackets', function () {
+        test('a{ size: calc(4 * 2px)  2px; }',
+             'a{ width: calc(4 * 2px); height: 2px; }');
+    });
+
 });
