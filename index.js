2b2b28a7cc142274c43bfb2236ceda42b9e8fbe1
Andrey Sitnik <andrey@sitnik.ru>
2015-05-05 05:39:59 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var list = require('postcss/lib/list');
 
 module.exports = function (css) {
-    css.eachDecl('size', function (decl, i) {
+    css.eachDecl('size', function (decl) {
         var sizes = list.space(decl.value);
-        if ( sizes.length == 1 ) sizes[1] = sizes[0];
+        if ( sizes.length === 1 ) sizes[1] = sizes[0];
 
         decl.cloneBefore({ prop: 'width',  value: sizes[0] });
         decl.cloneBefore({ prop: 'height', value: sizes[1] });
