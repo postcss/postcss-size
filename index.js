31b69f51998902f6bb0e33f414be03fe563f0dcd
Andrey Sitnik <andrey@sitnik.ru>
2015-08-23 15:35:10 +0300

    Use PostCSS 5.0 API

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var postcss = require('postcss');
 
 module.exports = postcss.plugin('postcss-size', function () {
     return function (css) {
-        css.eachDecl('size', function (decl) {
+        css.walkDecls('size', function (decl) {
             var sizes = postcss.list.space(decl.value);
             if ( sizes.length === 1 ) sizes[1] = sizes[0];
 
             decl.cloneBefore({ prop: 'width',  value: sizes[0] });
             decl.cloneBefore({ prop: 'height', value: sizes[1] });
 
-            decl.removeSelf();
+            decl.remove();
         });
     };
 });
