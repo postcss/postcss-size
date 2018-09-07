9b1f286ad665faa8ce4e2dadb1348c744001b6f6
Andrey Sitnik <andrey@sitnik.ru>
2018-09-07 16:23:36 +0300

    Update dependencies

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
 module.exports = postcss.plugin('postcss-size', function () {
-    return function (css) {
-        css.walkDecls('size', function (decl) {
-            if (decl.parent.name !== 'page') {
-                var sizes = postcss.list.space(decl.value);
-                if ( sizes.length === 1 ) sizes[1] = sizes[0];
+  return function (css) {
+    css.walkDecls('size', function (decl) {
+      if (decl.parent.name !== 'page') {
+        var sizes = postcss.list.space(decl.value)
+        if (sizes.length === 1) sizes[1] = sizes[0]
 
-                decl.cloneBefore({ prop: 'width',  value: sizes[0] });
-                decl.cloneBefore({ prop: 'height', value: sizes[1] });
+        decl.cloneBefore({ prop: 'width', value: sizes[0] })
+        decl.cloneBefore({ prop: 'height', value: sizes[1] })
 
-                decl.remove();
-            }
-        });
-    };
-});
+        decl.remove()
+      }
+    })
+  }
+})
--- index.test.js
+++ index.test.js
@@ -1,42 +1,43 @@
-var postcss = require('postcss');
+var postcss = require('postcss')
 
-var plugin = require('./');
+var plugin = require('./')
 
-function run(input, output) {
-    return postcss([ plugin ]).process(input).then(result => {
-        expect(result.css).toEqual(output);
-        expect(result.warnings().length).toBe(0);
-    });
+function run (input, output) {
+  return postcss([plugin]).process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-it('sets width and height', () => {
-    return run('a{ size: 1px 2px; }', 'a{ width: 1px; height: 2px; }');
-});
+it('sets width and height', function () {
+  return run('a{ size: 1px 2px; }', 'a{ width: 1px; height: 2px; }')
+})
 
-it('sets width and height by one value', () => {
-    return run('a{ size: 1px; }', 'a{ width: 1px; height: 1px; }');
-});
+it('sets width and height by one value', function () {
+  return run('a{ size: 1px; }', 'a{ width: 1px; height: 1px; }')
+})
 
-it('splits values by brackets', () => {
-    return run('a{ size: calc(4 * 2px)  2px; }',
-               'a{ width: calc(4 * 2px); height: 2px; }');
-});
+it('splits values by brackets', function () {
+  return run('a{ size: calc(4 * 2px)  2px; }',
+    'a{ width: calc(4 * 2px); height: 2px; }')
+})
 
-it('prefix value', () => {
-    return run('a{ size: -webkit-fit-content  auto; }',
-               'a{ width: -webkit-fit-content; height: auto; }');
-});
+it('prefix value', function () {
+  return run('a{ size: -webkit-fit-content  auto; }',
+    'a{ width: -webkit-fit-content; height: auto; }')
+})
 
-it('supports auto value', () => {
-    return run('a{ size: .98%  auto; }', 'a{ width: .98%; height: auto; }');
-});
+it('supports auto value', function () {
+  return run('a{ size: .98%  auto; }', 'a{ width: .98%; height: auto; }')
+})
 
-it('supports !important', () => {
-    return run('a{ size: 1px !important; }',
-               'a{ width: 1px !important; height: 1px !important; }');
-});
+it('supports !important', function () {
+  return run('a{ size: 1px !important; }',
+    'a{ width: 1px !important; height: 1px !important; }')
+})
 
-it('not conflicts with @page size descriptor', () => {
-    return run('@page{ size: 4cm 6cm landscape; }',
-               '@page{ size: 4cm 6cm landscape; }');
-});
+it('not conflicts with @page size descriptor', function () {
+  return run('@page{ size: 4cm 6cm landscape; }',
+    '@page{ size: 4cm 6cm landscape; }')
+})
