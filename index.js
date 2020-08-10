125084a9386ebe51a45d17592b6c906920b3f54c
Andrey Sitnik <andrey@sitnik.ru>
2020-08-09 21:00:24 -0700

    Use PostCSS 8

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let postcss = require('postcss')
-
-module.exports = postcss.plugin('postcss-size', () => {
-  return css => {
-    css.walkDecls('size', decl => {
-      if (decl.parent.name !== 'page') {
-        let sizes = postcss.list.space(decl.value)
+module.exports = () => {
+  return {
+    postcssPlugin: 'postcss-size',
+    Declaration (decl, { list }) {
+      if (decl.prop === 'size' && decl.parent.name !== 'page') {
+        let sizes = list.space(decl.value)
         if (sizes.length === 1) sizes[1] = sizes[0]
 
         decl.cloneBefore({ prop: 'width', value: sizes[0] })
@@ -12,6 +11,7 @@ module.exports = postcss.plugin('postcss-size', () => {
 
         decl.remove()
       }
-    })
+    }
   }
-})
+}
+module.exports.postcss = true
