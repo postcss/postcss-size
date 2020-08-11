6227646c2f0793c1546f4e3c8ecd247e16ac23f9
Andrey Sitnik <andrey@sitnik.ru>
2020-08-10 22:46:30 -0700

    Speed up declaration search

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 module.exports = () => {
   return {
     postcssPlugin: 'postcss-size',
-    Declaration (decl, { list }) {
-      if (decl.prop === 'size' && decl.parent.name !== 'page') {
-        let sizes = list.space(decl.value)
-        if (sizes.length === 1) sizes[1] = sizes[0]
+    Declaration: {
+      size: (decl, { list }) => {
+        if (decl.parent.name !== 'page') {
+          let sizes = list.space(decl.value)
+          if (sizes.length === 1) sizes[1] = sizes[0]
 
-        decl.cloneBefore({ prop: 'width', value: sizes[0] })
-        decl.cloneBefore({ prop: 'height', value: sizes[1] })
+          decl.cloneBefore({ prop: 'width', value: sizes[0] })
+          decl.cloneBefore({ prop: 'height', value: sizes[1] })
 
-        decl.remove()
+          decl.remove()
+        }
       }
     }
   }
