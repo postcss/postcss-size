7c3f47e7c28116d44eef7acfb2e1955c8ccdf798
Mehdi M <hi@mehdi.cc>
2023-10-13 12:27:36 +0200

    Avoid code repetition

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
 module.exports = () => {
+  function processSize (propPrefix = '', decl, list) {
+    let sizes = list.space(decl.value)
+    if (sizes.length === 1) sizes[1] = sizes[0]
+
+    decl.cloneBefore({ prop: propPrefix + 'width', value: sizes[0] })
+    decl.cloneBefore({ prop: propPrefix + 'height', value: sizes[1] })
+
+    decl.remove()
+  }
+
   return {
     postcssPlugin: 'postcss-size',
     Declaration: {
       'size': (decl, { list }) => {
         if (decl.parent.name !== 'page') {
-          let sizes = list.space(decl.value)
-          if (sizes.length === 1) sizes[1] = sizes[0]
-
-          decl.cloneBefore({ prop: 'width', value: sizes[0] })
-          decl.cloneBefore({ prop: 'height', value: sizes[1] })
-
-          decl.remove()
+          processSize('', decl, list)
         }
       },
-      'min-size': (decl, { list }) => {
-        let sizes = list.space(decl.value)
-        if (sizes.length === 1) sizes[1] = sizes[0]
 
-        decl.cloneBefore({ prop: 'min-width', value: sizes[0] })
-        decl.cloneBefore({ prop: 'min-height', value: sizes[1] })
+      'min-size': (decl, { list }) => processSize('min-', decl, list),
 
-        decl.remove()
-      },
-      'max-size': (decl, { list }) => {
-        let sizes = list.space(decl.value)
-        if (sizes.length === 1) sizes[1] = sizes[0]
-
-        decl.cloneBefore({ prop: 'max-width', value: sizes[0] })
-        decl.cloneBefore({ prop: 'max-height', value: sizes[1] })
-
-        decl.remove()
-      }
+      'max-size': (decl, { list }) => processSize('max-', decl, list)
     }
   }
 }
